3ad6d6fbf0cf397c536c2465bbd059eeeae2c538
Jonathan <jchiangconan@gmail.com>
2023-06-30 19:45:06 -0400

    google sheets API downloads books to CSV format

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # LibChat
 Let Cavendish Labs Library recommend you books!
+
+# Setup
+1. Clone the repository
+2. Install the dependencies
+3. Follow the Google Sheets API Python Quickstart guide to set up your Google Sheets API credentials, make sure to save the credentials file as `credentials.json` in the root directory of the repository
+4. The first time you run the program, you will be prompted to log in to your Google account, and give the program permission to access your Google Sheets
--- main.py
+++ main.py
@@ -1,16 +1,54 @@
-# This is a sample Python script.
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1JFyivbZzcRjRE-ImCZBT-D9IILZ7JEL9aUyII8GE5q4'
+SAMPLE_RANGE_NAME = "Books"
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def save_to_csv():
+    creds = Credentials.from_authorized_user_file('token.json', SCOPES) if os.path.exists("token.json") else None
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+
+        values = service.spreadsheets().values().get(
+            spreadsheetId=SAMPLE_SPREADSHEET_ID,
+            range=SAMPLE_RANGE_NAME
+        ).execute().get('values', [])
+
+        if not values:
+            print('No data found.')
+            return
+
+        headers = values.pop(0)
+        for row in values:
+            print(row)
+
+        # save to csv file
+        with open('books.csv', 'w') as f:
+            f.write(','.join(headers) + '\n')
+            for row in values:
+                f.write(','.join(row) + '\n')
+    except HttpError as err:
+        print(err)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    save_to_csv()
